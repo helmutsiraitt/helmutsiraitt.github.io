df2eb069298d19631d7a12877743f2e309006a1a
helmut4646 <elmudfile@gmail.com>
2024-09-21 04:35:45 +0700

    style.css: still fixed responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,52 @@
 // navigation
 const nav = document.getElementsByTagName('nav')[0];
-// nav.addEventListener('click', function(e) {
-//       if(e.target.tagName == 'svg'){
-//            e.target.classList.toggle('active');
-//            e.preventDefault();
-//       }
-// });
-const svg = nav.getElementsByTagName('svg')[0];
-const navUl = nav.querySelector('nav ul');
-svg.addEventListener('click', function(e) {
-    e.target.classList.toggle('active');
-    e.target.previousElementSibling.classList.toggle("active");
+const hamBtn = nav.getElementsByClassName('hamburger-btn')[0];
+hamBtn.addEventListener('click', function() {
+        hamBtn.classList.toggle('active');
+        hamBtn.previousElementSibling.classList.toggle('active');
+        document.body.classList.toggle('active');
 });
 
+// change background menu to scroll
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 20) { 
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
 
+// smoothscrolling when click the link 
+const smoothScroll = function (targetEl, duration) {
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - nav.clientHeight;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+    
+        const ease = function(t,b,c,d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+    
+        const animation = function(currentTime){
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+    };
 
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.nav-link');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
