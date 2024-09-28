62339b178eef519d0d0b7e44911b24c01aebbead
helmutsiraitt <elmutsirait96@gmail.com>
2024-09-28 07:47:12 +0700

    style.css: add active link in navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 // navigation
 const nav = document.getElementsByTagName('nav')[0];
 const hamBtn = nav.getElementsByClassName('hamburger-btn')[0];
+const links = document.querySelectorAll('.nav-link');
+const navUl = document.querySelector('nav ul');
+const sections = document.querySelectorAll('section');
+
 hamBtn.addEventListener('click', function() {
         hamBtn.classList.toggle('active');
         hamBtn.previousElementSibling.classList.toggle('active');
@@ -41,8 +45,6 @@ const smoothScroll = function (targetEl, duration) {
     };
 
     const scrollTo = function () {
-        const links = document.querySelectorAll('.nav-link');
-        const navUl = document.querySelector('nav ul');
         links.forEach(each => {
             each.addEventListener('click', function (e) {
                 const currentTarget = this.getAttribute('href');
@@ -55,3 +57,19 @@ const smoothScroll = function (targetEl, duration) {
     
     };
     scrollTo();
+
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+            links.forEach(event => {
+                event.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
